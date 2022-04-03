40884c6a58d5f18854f0af6475dec96f113afce4
Leonel López Pisani <leolopezpisani@gmail.com>
2022-04-03 19:51:10 -0300

    Tasks mock added

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import { Task } from "./Task"
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Terminar primer módulo de Angular",
+        day: '10 de abril a las 12:00',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Terminar segundo módulo de Angular",
+        day: '20 de abril a las 15:00',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Terminar tercer módulo de Angular",
+        day: '30 de abril a las 19:00',
+        reminder: false
+    },
+    {
+        id: 4,
+        text: "Terminar cuarto módulo de Angular",
+        day: '10 de mayo a las 7:00',
+        reminder: false
+    }
+]
